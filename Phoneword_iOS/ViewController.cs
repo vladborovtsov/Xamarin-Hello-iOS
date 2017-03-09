40028c0dc1b683ae1b0e987dc7c5f1c76a0092da
Vlad Borovtsov <borovcov.vlad@gmail.com>
2017-03-09 13:46:32 +0700

    base stuff described in tutorial; builds successful

--- Phoneword_iOS/ViewController.cs
+++ Phoneword_iOS/ViewController.cs
@@ -2,6 +2,8 @@
 
 using UIKit;
 
+using Foundation;
+
 namespace Phoneword_iOS
 {
 	public partial class ViewController : UIViewController
@@ -15,6 +17,37 @@ namespace Phoneword_iOS
 		{
 			base.ViewDidLoad();
 			// Perform any additional setup after loading the view, typically from a nib.
+
+			string translatedNumber = "";
+			TranslateButton.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				translatedNumber = PhoneTranslator.ToNumber(PhoneNumberText.Text);
+				PhoneNumberText.ResignFirstResponder();
+
+				if (translatedNumber == "")
+				{
+					CallButton.SetTitle("Call ", UIControlState.Normal);
+					CallButton.Enabled = false;
+				}
+				else
+				{
+					CallButton.SetTitle("Call " + translatedNumber, UIControlState.Normal);
+					CallButton.Enabled = true; 
+				}
+			};
+
+			CallButton.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				var url = new NSUrl("tel:" + translatedNumber);
+				//...otherwise show an alert dialog. 
+				if (!UIApplication.SharedApplication.OpenUrl(url))
+				{
+					var alert = UIAlertController.Create("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+					alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController(alert, true, null);
+				}
+
+			};
 		}
 
 		public override void DidReceiveMemoryWarning()
